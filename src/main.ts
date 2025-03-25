d015b7fb2144ecd68d8759ed1212a4e64ce8c700
Andtit4 <andtit4@gmail.com>
2025-03-25 12:33:29 +0000

    test rebuild vercel

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 // import { UserRepository } from './infrastructure/repository/user.repository';
 // import { CreateUserUseCase } from './application/use-cases/create-user.use-case';
 import * as dotenv from 'dotenv';
-// import { MongooseModule } from '@nestjs/mongoose';
 
 dotenv.config();
 
